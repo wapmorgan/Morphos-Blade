7fa4b251f7865d325e4f2ae43b92f7874ec7d013
wapmorgan <wapmorgan@gmail.com>
2017-02-03 00:17:09 +0300

    FIrst

---
+++ BladeProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace morphos;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class BladeProvider extends ServiceProvider {
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('plural', function ($expression) {
+            return "<?php echo morphos\\Russian\\Plurality::pluralize($expression); ?>";
+        });
+
+        Blade::directive('name', function ($expression) {
+            return "<?php echo morphos\\Russian\\nameCase($expression); ?>";
+        });
+    }
+
+    public function register(){}
+}
