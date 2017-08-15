097fd43c6903c3c7a6d6959b14e072e314773a30
wapmorgan <wapmorgan@gmail.com>
2017-08-15 23:47:24 +0300

    Update Morphos version

--- MorphosBladeProvider.php
+++ MorphosBladeProvider.php
@@ -16,8 +16,7 @@ class MorphosBladeProvider extends ServiceProvider {
     public function boot() {
         // @plural(count, noun)
         Blade::directive('plural', function ($expression) {
-            $expression = array_reverse(explode(',', $expression));
-            return '<?php echo ('.$expression[1].').\' \'.morphos\\Russian\\Plurality::pluralize('.implode(',', $expression).'); ?>';
+            return '<?php echo ('.$expression[1].').\' \'.morphos\\Russian\\pluralize('.$expression.'); ?>';
         });
 
         // @numeral(number)
@@ -27,9 +26,9 @@ class MorphosBladeProvider extends ServiceProvider {
         Blade::directive('numeral', function ($expression) {
             $expression = explode(',', $expression);
             if (count($expression) == 1 || in_array(trim($expression[1]), array('\'f\'', '\'m\'', '\'n\'')))
-                return '<?php echo morphos\\Russian\\CardinalNumeral::getCase('.$expression[0].', morphos\\Cases::NOMINATIVE'.(isset($expression[1]) ? ' ,'.$expression[1] : null).') ?>';
+                return '<?php echo \\morphos\\Russian\\CardinalNumeralGenerator::getCase('.$expression[0].', \\morphos\\Cases::NOMINATIVE'.(isset($expression[1]) ? ' ,'.$expression[1] : null).') ?>';
             else
-                return '<?php echo morphos\\Russian\\CardinalNumeral::getCase('.$expression[0].', morphos\\Cases::NOMINATIVE'.(isset($expression[2]) ? ' ,'.$expression[2] : null).') ?> <?php echo morphos\\Russian\\Plurality::pluralize('.$expression[1].','.$expression[0].'); ?>';
+                return '<?php echo \\morphos\\Russian\\CardinalNumeralGenerator::getCase('.$expression[0].', \\morphos\\Cases::NOMINATIVE'.(isset($expression[2]) ? ' ,'.$expression[2] : null).') ?> <?php echo morphos\\Russian\\NounPluralization::pluralize('.$expression[1].','.$expression[0].'); ?>';
         });
 
         // @ordinal(number)
@@ -37,61 +36,15 @@ class MorphosBladeProvider extends ServiceProvider {
         Blade::directive('ordinal', function ($expression) {
             $expression = explode(',', $expression);
             if (count($expression) == 2)
-                return '<?php echo morphos\\Russian\\OrdinalNumeral::getCase('.$expression[0].', morphos\\Cases::NOMINATIVE, '.$expression[1].') ?>';
+                return '<?php echo \\morphos\\Russian\\OrdinalNumeralGenerator::getCase('.$expression[0].', \\morphos\\Cases::NOMINATIVE, '.$expression[1].') ?>';
             else
-                return '<?php echo morphos\\Russian\\OrdinalNumeral::getCase('.$expression[0].', morphos\\Cases::NOMINATIVE) ?>';
+                return '<?php echo \\morphos\\Russian\\OrdinalNumeralGenerator::getCase('.$expression[0].', \\morphos\\Cases::NOMINATIVE) ?>';
         });
 
         // @money(value, currency)
         Blade::directive('money', function ($expression) {
             $expression = explode(',', $expression);
-            $money = '(float)trim('.$expression[0].', \'\\\'\')';
-            $currency = mb_strtolower(trim($expression[1], '\' '));
-            $money_big = 'floor('.$money.')';
-            $money_little = 'floor('.$money.' * 100 % 100)';
-            switch ($currency) {
-                case '₽':
-                case 'р':
-                case 'рубль':
-                case 'r':
-                case 'rub':
-                    $currency = array('рубль', 'копейка');
-                    break;
-
-                case '₴':
-                case 'г':
-                case 'гривна':
-                case 'uah':
-                    $currency = array('гривна', 'копейка');
-                    break;
-
-                case '$':
-                case 'доллар':
-                case 'u':
-                case 'usd':
-                    $currency = array('доллар', 'цент');
-                    break;
-
-                case '€':
-                case 'евро':
-                case 'e':
-                case 'eur':
-                case 'euro':
-                    $currency = array('евро', 'цент');
-                    break;
-
-                case '£':
-                case 'фунт':
-                case 'gbp':
-                    $currency = array('фунт', 'пенни');
-                    break;
-
-                default:
-                    return '<?php echo ('.$expression[0].').\' \'.('.$expression[1].') ?>';
-            }
-
-            return '<?php echo '.$money_big.'.\' \'.morphos\\Russian\\Plurality::pluralize(\''.$currency[0].'\', '.$money_big.') ?> <?php echo '.$money_little.'.\' \'.morphos\\Russian\\Plurality::pluralize(\''.$currency[1].'\', '.$money_little.') ?>';
-
+            return '<?php echo \\morphos\\Russian\\MoneySpeller::spell('.implode(',', $expression).', \\morphos\\Russian\\MoneySpeller::SHORT_FORMAT) ?>';
         });
 
         // @name(name, case)
@@ -99,9 +52,9 @@ class MorphosBladeProvider extends ServiceProvider {
         Blade::directive('name', function ($expression) {
             $expression = explode(',', $expression);
             if (count($expression) == 2)
-                return '<?php echo morphos\\Russian\\name('.$expression[0].','.$expression[1].'); ?>';
+                return '<?php echo \\morphos\\Russian\\inflectName('.$expression[0].','.$expression[1].'); ?>';
             else
-                return '<?php echo morphos\\Russian\\name('.$expression[0].','.$expression[2].','.$expression[1].'); ?>';
+                return '<?php echo \\morphos\\Russian\\inflectName('.$expression[0].','.$expression[2].','.$expression[1].'); ?>';
         });
     }
 
